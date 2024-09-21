c4ece7a2a24d1ecb1b103ea528a1d651baf06ed5
AngelSeraphim <server@praniksl.ru>
2024-09-21 23:19:40 +0500

    Permissions fix

--- ColorTag/Config.cs
+++ ColorTag/Config.cs
@@ -11,9 +11,9 @@ namespace ColorTag
         public int MaxColorLimit { get; set; } = 5;
 
         [Description("Rights so that the player can change his colors")]
-        public string ColorRequirePermission = "colortag.get";
+        public string ColorRequirePermission { get; set; } = "colortag.get";
 
         [Description("Rights to remove colors from a player")]
-        public string AdminRequirePermission = "colortag.admin";
+        public string AdminRequirePermission { get; set; } = "colortag.admin";
     }
 }
